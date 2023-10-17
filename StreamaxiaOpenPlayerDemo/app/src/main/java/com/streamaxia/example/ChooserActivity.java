08f0c71b42099036b2583146d8c3c0310cee9170
Razvan Chelemen <chelemen.razvan@gmail.com>
2023-10-17 11:17:52 +0300

    Update API level

--- StreamaxiaOpenPlayerDemo/app/src/main/java/com/streamaxia/example/ChooserActivity.java
+++ StreamaxiaOpenPlayerDemo/app/src/main/java/com/streamaxia/example/ChooserActivity.java
@@ -22,11 +22,9 @@ public class ChooserActivity extends Activity {
     public static final String TYPE = "type";
 
     // Modify this to your desired streams
-    public static final String RTMP_URL = "rtmp://184.72.239.149/vod/mp4:bigbuckbunny_1500.mp4";
-    public static final String HLS_URL = "https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8";
-    public static final String DASH_URL = "http://www.youtube.com/api/manifest/dash/id/3aa39fa2cc27967f/source/youtube?as=fmp4_audio_clear,fmp4_sd_hd_clear&sparams="
-            + "ip,ipbits,expire,source,id,as&ip=0.0.0.0&ipbits=0&expire=19000000000&signature=A2716F75795F5D2AF0E88962FFCD10DB79384F29.84308FF04844498" +
-            "CE6FBCE4731507882B8307798&key=ik0";
+    public static final String RTMP_URL = "rtmp://vd2.wmspanel.com/video_demo/stream";
+    public static final String HLS_URL = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+    public static final String DASH_URL = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
 
     @BindView(R.id.rtmp_demo)
     RelativeLayout rtmpDemoBtn;
